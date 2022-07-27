39750fd69ca6ddd0907a004d6baa280e2c817b55
Lautaro <lautafuriasse16@gmail.com>
2022-07-26 23:04:30 -0300

    fix

---
+++ src/main/java/com/alkemy/disney/Security/Controller/UserAuthController.java
@@ -0,0 +1,57 @@
+package com.alkemy.disney.Security.Controller;
+
+import com.alkemy.disney.Security.Dto.AuthenticationRequest;
+import com.alkemy.disney.Security.Dto.AuthenticationResponse;
+import com.alkemy.disney.Security.Dto.UserDto;
+import com.alkemy.disney.Security.Service.JwtUtils;
+import com.alkemy.disney.Security.Service.UserDetailsCustomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class UserAuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserDetailsCustomService userDetailsCustomService;
+    @Autowired
+    private JwtUtils jwtTokenUtil;
+
+
+    @PostMapping("/singup")
+    public ResponseEntity<AuthenticationResponse> singUp(@Valid @RequestBody UserDto userDto) throws Exception{
+        userDetailsCustomService.save(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/singin")
+    public ResponseEntity<AuthenticationResponse> singIn(@RequestBody AuthenticationRequest authenticationRequest)
+            throws Exception{
+        UserDetails userDetails;
+        try {
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
+                            authenticationRequest.getPassword())
+            );
+            userDetails = (UserDetails)auth.getPrincipal();
+        }catch (BadCredentialsException e){
+            throw new Exception("Incorrect username or password");
+        }
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+
+}
---
+++ src/main/java/com/alkemy/disney/Security/Dto/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package com.alkemy.disney.Security.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String jwt;
+}
